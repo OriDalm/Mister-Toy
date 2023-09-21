1b8eca65f1e804292cbb6819e40ee7abacd8d853
OriDalm <oridalmedigos@gmail.com>
2023-09-21 13:10:54 +0300

    Added header, empty home, about and dashboard page

---
+++ src/cmps/AppHeader.jsx
@@ -0,0 +1,17 @@
+import { Link, NavLink } from 'react-router-dom'
+
+export function AppHeader() {
+  return (
+    <header className='app-header'>
+      <Link className='logo' to='/'>
+        <h2>Mister Toys</h2>
+      </Link>
+      <nav className='nav-links'>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/about'>About</NavLink>
+        <NavLink to='/toy'>Toys</NavLink>
+        <NavLink to='/dashboard'>Dashboard</NavLink>
+      </nav>
+    </header>
+  )
+}

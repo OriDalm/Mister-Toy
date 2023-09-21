99d4d20f28632ab76e0edc550fe9314940bf6bf3
OriDalm <oridalmedigos@gmail.com>
2023-09-21 13:26:45 +0300

    Designed home and about pages

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,3 +1,8 @@
 export function AboutUs() {
-  return <div>About us</div>
+  return (
+    <h1>
+      Welcome to Toy Exhibition Wonderland, where imagination comes to life! We are thrilled to be your gateway to a world of wonder, nostalgia, and
+      endless fun. Our mission is to celebrate the magic of toys and their enduring impact on people of all ages.
+    </h1>
+  )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,10 @@
+import logoUrl from '../assets/img/toy.jpg'
+
 export function HomePage() {
-  return <div>Home Page</div>
+  return (
+    <section>
+      <h1>Welcome To My Toy Exhibition</h1>
+      <img className='toy-img' src={logoUrl} />
+    </section>
+  )
 }

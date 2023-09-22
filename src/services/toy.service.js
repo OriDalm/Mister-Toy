50b438505ba5a575bb233d139f11e3af8923cc6b
OriDalm <oridalmedigos@gmail.com>
2023-09-22 17:50:26 +0300

    ajax fixes

--- src/services/toy.service.js
+++ src/services/toy.service.js
@@ -1,4 +1,5 @@
 import { storageService } from './async-storage.service.js'
+import { httpService } from './http.service.js'
 import { utilService } from './util.service.js'
 
 const STORAGE_KEY = 'toyDB'

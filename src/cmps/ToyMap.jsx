abe50d7b7363e11b1bf1f1363dadc255f218a195
OriDalm <oridalmedigos@gmail.com>
2023-09-25 16:50:54 +0300

    added map in about page

---
+++ src/cmps/ToyMap.jsx
@@ -0,0 +1,35 @@
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
+import { useState } from 'react'
+
+const DOTS = 1
+const positions = [
+  { lat: 32.0853, lng: 34.7818 },
+  { lat: 32.434, lng: 34.9197 },
+  { lat: 32.0132, lng: 34.748 },
+]
+export function ToyMap() {
+  const [map, setMap] = useState()
+
+  return (
+    <LoadScript id='script-loader' googleMapsApiKey='API_KEY_HERE'>
+      <div className='App-map' style={{ height: '400px', width: '100%' }}>
+        <GoogleMap
+          options={{
+            disableDefaultUI: true,
+            gestureHandling: 'greedy',
+          }}
+          mapContainerStyle={{ height: '100%', width: '100%' }}
+          center={{ lat: 32.0853, lng: 34.7818 }}
+          zoom={10}
+          onLoad={(map) => {
+            setMap(map)
+          }}
+        >
+          {positions.map((position, index) => (
+            <Marker key={index} position={position} onClick={() => map && map.setCenter(position)} />
+          ))}
+        </GoogleMap>
+      </div>
+    </LoadScript>
+  )
+}
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,8 +1,11 @@
+import { ToyMap } from '../cmps/ToyMap'
+
 export function AboutUs() {
   return (
     <h1>
       Welcome to Toy Exhibition Wonderland, where imagination comes to life! We are thrilled to be your gateway to a world of wonder, nostalgia, and
       endless fun. Our mission is to celebrate the magic of toys and their enduring impact on people of all ages.
+      <ToyMap />
     </h1>
   )
 }
